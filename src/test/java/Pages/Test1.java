43da09812066df2bc32379ce230447a7002931c1
Monika Korus <moko@ailleron.com>
2018-11-21 10:21:19 +0100

    first commit

--- src/test/java/Pages/Test1.java
+++ src/test/java/Pages/Test1.java
@@ -1,9 +1,18 @@
 package Pages;
 
+import io.qameta.allure.Description;
+import io.qameta.allure.Epic;
+import io.qameta.allure.Feature;
+import io.qameta.allure.Story;
 import org.testng.annotations.Test;
+
+@Epic("Test epic")
+@Feature("Test feature")
 
 public class Test1 {
     @Test(groups = {"smoke","regresion"})
+    @Story("This is skmoke test")
+    @Description("This test will")
     public void testName() {
 
     }
